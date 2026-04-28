b1898e1a98f7c21db44452b11c99d673b03085e8
Kyeongmin Kim <kkfenix3085@naver.com>
2026-04-28 14:30:23 +0900

    Set Packet Header

--- Server/AuctionGameServer/Protocol.h
+++ Server/AuctionGameServer/Protocol.h
@@ -5,11 +5,25 @@
 #endif
 
 #define SERVER_ADDRESS L"0.0.0.0"
-#define SERVER_PORT 6000 //11850
+#define SERVER_PORT 12001 //11850
 
-#define SESSION_MAX 10000
+#define SESSION_MAX 18000
 
-typedef struct EchoPacketHeader
+#define PACKET_CODE 0x77
+#define PACKET_KEY 0x32
+
+#pragma pack(push, 1)
+typedef struct NetPacketHeader
 {
-	short _len;
-} EchoPacketHeader;
+	unsigned char _code = PACKET_CODE;
+	unsigned short _len = 0;
+	unsigned char _randKey = 0;
+	unsigned char _checkSum = 0;
+} NetPacketHeader;
+#pragma pack(pop)
+
+#define PACKET_HEADER_SIZE sizeof(NetPacketHeader)
+#define PACKET_SIZE 1024
+#define PACKET_MAX_SIZE 8192
+
+using SessionID = unsigned __int64;
